360e887a9c51b1f3931efe21d6d80e2db2896c88
Rijpstra <rijpstra@rdw.nl>
2023-05-03 15:39:37 +0200

    1

--- test2.py
+++ test2.py
@@ -4,7 +4,8 @@ from pandas import read_csv
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot
-st.title('DCI test app help3243')
+from statsmodels.tsa.arima.model import ARIMA
+st.title('DCI test app help5')
 
 uploaded_file = st.file_uploader('Upload a CSV')
     # Can be used wherever a "file-like" object is accepted:
@@ -17,8 +18,12 @@ def parser(x):
  return datetime.strptime('190'+x, '%Y-%m')
  
 series = read_csv(dataframe, header=0, parse_dates=[0], index_col=0, date_parser=parser)
+series.index = series.index.to_period('M')
+# fit model
+model = ARIMA(series, order=(5,1,0))
+model_fit = model.fit()
 print(series.head())
-test = series.plot()
+series.plot()
 pyplot.show()
 
-st.pyplot(test)
+st.pyplot()
