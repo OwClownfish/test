993331fa907dc275049f93b9f8fcad5e2e2171ec
Rijpstra <rijpstra@rdw.nl>
2023-05-03 14:56:45 +0200

    1

--- test.py
+++ test.py
@@ -8,9 +8,16 @@ uploaded_file = st.file_uploader('Upload a CSV')
     # Can be used wherever a "file-like" object is accepted:
 dataframe = pd.read_csv(uploaded_file)
 
-
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+ 
+def parser(x):
+ return datetime.strptime('190'+x, '%Y-%m')
+ 
+series = read_csv(dataframe, header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+print(series.head())
+test = series.plot()
+pyplot.show()
 #
-
-st.write(dataframe)
-st.line_chart(dataframe,x="schorsingen", y="datum")
-st.table(dataframe)
+st.pyplot(test)
