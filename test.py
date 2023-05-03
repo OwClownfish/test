7d58cb5d0aa7491b4a83962708c274bf2b884ec5
Rijpstra <rijpstra@rdw.nl>
2023-05-03 14:28:06 +0200

    1

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ import numpy as np
 
 st.title('DCI test app')
 
+st.file_uploader('Upload a CSV')
+
 df = pd.DataFrame(
    np.random.randn(10, 5),
    columns=('col %d' % i for i in range(5)))
-
+st.line_chart(df)
 st.table(df)
