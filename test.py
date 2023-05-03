d07564f96ac01befce6cdbc32be60c806c68179e
Rijpstra <rijpstra@rdw.nl>
2023-05-03 14:44:55 +0200

    1

--- test.py
+++ test.py
@@ -7,7 +7,11 @@ st.title('DCI test app help')
 uploaded_file = st.file_uploader('Upload a CSV')
     # Can be used wherever a "file-like" object is accepted:
 dataframe = pd.read_csv(uploaded_file)
-st.write(dataframe)
 
-st.line_chart(dataframe)
-st.table(dataframe,x="schorsingen", y="datum")
+
+#st.write(dataframe)
+try:
+    st.line_chart(dataframe,x="schorsingen", y="datum")
+    st.table(dataframe)
+except:
+    pass
