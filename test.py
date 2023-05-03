6fb3c75444a07b744de92618d4f980e7ea32f5fd
Rijpstra <rijpstra@rdw.nl>
2023-05-03 14:45:56 +0200

    1

--- test.py
+++ test.py
@@ -9,8 +9,9 @@ uploaded_file = st.file_uploader('Upload a CSV')
 dataframe = pd.read_csv(uploaded_file)
 
 
-#st.write(dataframe)
+#
 try:
+    st.write(dataframe)
     st.line_chart(dataframe,x="schorsingen", y="datum")
     st.table(dataframe)
 except:
