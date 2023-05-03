dacff69ea9f4e746edec4e977b5ecaef946b7db8
Rijpstra <rijpstra@rdw.nl>
2023-05-03 15:27:13 +0200

    1

---
+++ test2.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import datetime
+from pandas import read_csv
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot
+st.title('DCI test app help3243')
+
+uploaded_file = st.file_uploader('Upload a CSV')
+    # Can be used wherever a "file-like" object is accepted:
+dataframe = pd.read_csv(uploaded_file)
+
+
+st.write(dataframe)
+ 
+def parser(x):
+ return datetime.strptime('190'+x, '%Y-%m')
+ 
+series = read_csv(dataframe, header=0, parse_dates=[0], index_col=0, date_parser=parser)
+print(series.head())
+test = series.plot()
+pyplot.show()
+
+st.pyplot(test)
